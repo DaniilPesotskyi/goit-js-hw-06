098b976719c31879b552ffa180e8e0a925df158f
Daniil Pesotskyi <pesotckiydanya@gmail.com>
2023-02-24 01:00:16 +0200

    task 5

---
+++ js/task-05.js
@@ -0,0 +1,8 @@
+const inputEl = document.getElementById('name-input')
+const outputEl = document.getElementById('name-output')
+
+
+inputEl.addEventListener('input', event => {
+
+    outputEl.textContent = event.currentTarget.value
+})
